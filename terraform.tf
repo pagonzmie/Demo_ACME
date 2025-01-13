aa30419d64e391671c969052ddd0b723d138ffa3
pagonzmie <pablo.gonzalezmieres@hashicorp.com>
2025-01-13 12:48:41 +0100

    first commit

---
+++ terraform.tf
@@ -0,0 +1,23 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+
+  cloud {
+    organization = "ACMEDemo_Pablo"
+
+    workspaces {
+      name = "sandbox"
+      project = "sandbox"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.31.0"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
